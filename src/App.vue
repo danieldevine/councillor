b23fb6b5d900bade69326c63d545493deb704b32
Dan Devine <dan@maverick-intl.com>
2019-05-25 23:06:22 +0100

    get the party started

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="app">
+        <router-view/>
+    </div>
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Heebo:500,900&display=swap");
+@mixin base-typography {
+    font-family: "Heebo", sans-serif;
+    font-weight: 400;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+    line-height: 1.56;
+}
+* {
+    box-sizing: border-box;
+}
+html {
+    @include base-typography;
+    padding: 0;
+    margin: 0;
+}
+body {
+    overflow-x: hidden;
+    max-width: 100%;
+    height: 100% !important;
+    margin: 0 auto;
+    position: relative;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    margin: 0;
+}
+.councillor {
+    width: 100vw;
+    min-height: 100vh;
+    padding: 4rem;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: #cccccc;
+
+    .fleg {
+        position: fixed;
+        width: 100%;
+        display: grid;
+        height: 60px;
+        top: 0;
+        grid-template-columns: repeat(3, 1fr);
+        margin-top: 0;
+        margin-bottom: auto;
+
+        div {
+            background: white;
+            &:first-child {
+                background: rgb(24, 158, 24);
+            }
+
+            &:last-child {
+                background: rgb(240, 189, 48);
+            }
+        }
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="councillor">
+        <div class="fleg">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div v-if="surname">
+            <h4>Your councillor name is:</h4>
+            <h1>
+                {{firstname}}
+                <br>
+                "{{nickname}}"
+                <br>
+                {{surname}}
+            </h1>
+        </div>
+        <social-sharing
+            url="https://councillor-name/"
+            :title="`Vote #1 ${firstname} '${nickname}' ${surname}`"
+            :description="`Vote #1 ${firstname} '${nickname}' ${surname}` "
+            :quote="`Vote #1 ${firstname} '${nickname}' ${surname}` "
+            hashtags="councillorName"
+            twitter-user="coderjerk"
+            inline-template
+        >
+            <div class="councillor__social">
+                <network network="facebook">
+                    <i class="fa fa-facebook"></i> Facebook
+                </network>
+                <network network="twitter">
+                    <i class="fa fa-twitter"></i> Twitter
+                </network>
+            </div>
+        </social-sharing>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+import { log } from "util";
+
+export default {
+    name: "home",
+    components: {
+        // HelloWorld
+    },
+    data() {
+        return {
+            surname: "",
+            firstname: "",
+            nickname: ""
+        };
+    },
+    mounted() {
+        this.getSurname();
+        this.getFirstname();
+        this.getNickname();
+    },
+    methods: {
+        getSurname() {
+            axios
+                .get("/surnames.json")
+                .then(result => {
+                    let surnames = result.data.surnames;
+                    this.surname =
+                        surnames[Math.floor(Math.random() * surnames.length)];
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        },
+        getFirstname() {
+            axios
+                .get("/firstnames.json")
+                .then(result => {
+                    let firstnames = result.data.firstnames;
+                    this.firstname =
+                        firstnames[
+                            Math.floor(Math.random() * firstnames.length)
+                        ];
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        },
+        getNickname() {
+            axios
+                .get("/nicknames.json")
+                .then(result => {
+                    let nicknames = result.data.nicknames;
+                    this.nickname =
+                        nicknames[Math.floor(Math.random() * nicknames.length)];
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+};
+</script>
+<style lang="scss" scoped>
+h1 {
+    font-size: 8vw;
+    line-height: 1;
+}
+
+h4 {
+    font-size: 3vw;
+    font-weight: 400;
+}
+.councillor__social {
+    margin-top: 3rem;
+}
+</style>
+

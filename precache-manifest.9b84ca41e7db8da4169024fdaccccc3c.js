f08a9e143547988cb30d8754bc0863f33cf0872d
Dan Devine <dan@maverick-intl.com>
2019-05-27 20:51:27 +0100

    deploy

---
+++ precache-manifest.9b84ca41e7db8da4169024fdaccccc3c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "07bb1b095f5c1f2b8cfe07efb6c867ed",
+    "url": "/councillor/constituencies.json"
+  },
+  {
+    "revision": "711e742cc821e1efad6c",
+    "url": "/councillor/js/about.e3b1b4ab.js"
+  },
+  {
+    "revision": "3267e87431555043a600",
+    "url": "/councillor/js/app.1b743763.js"
+  },
+  {
+    "revision": "f1ba371400645cf00ba4",
+    "url": "/councillor/js/chunk-vendors.ef520942.js"
+  },
+  {
+    "revision": "b381817da167487e3339756170a80ad7",
+    "url": "/councillor/surnames.json"
+  },
+  {
+    "revision": "61e40fede86d0adf599ff448c3ac3c5a",
+    "url": "/councillor/nicknames.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/councillor/robots.txt"
+  },
+  {
+    "revision": "382ad424d0935e851f8145220d473605",
+    "url": "/councillor/index.html"
+  },
+  {
+    "revision": "9bcbae8a54b0f51dfde80889babdf67b",
+    "url": "/councillor/img/fatty.png"
+  },
+  {
+    "revision": "e376736ac06f490dee1f833ec6849efa",
+    "url": "/councillor/firstnames.json"
+  },
+  {
+    "revision": "3267e87431555043a600",
+    "url": "/councillor/css/app.cc8955d7.css"
+  }
+];
